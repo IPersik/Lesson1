6e47dba6d3efd1901a47223129e766967e3c9c65
Vera <ipeachyou@yandex.ru>
2022-02-20 14:33:21 +0500

    NetworkStatus

--- app/src/main/java/com/example/lesson1/App.kt
+++ app/src/main/java/com/example/lesson1/App.kt
@@ -1,6 +1,8 @@
 package com.example.lesson1
 
 import android.app.Application
+import android.util.Log
+import com.example.lesson1.network.NetworkStatus
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 
@@ -14,9 +16,15 @@ class App: Application() {
     val router
         get() = cicerone.router
 
+    private val networkStatus by lazy { NetworkStatus(this) }
+
     override fun onCreate() {
         super.onCreate()
         _instance = this
+
+        networkStatus.networkStatusSubject.subscribe {
+            Log.d("NetworkStatus", "Доступна ли сеть: $it")
+        }
     }
 
     companion object {
